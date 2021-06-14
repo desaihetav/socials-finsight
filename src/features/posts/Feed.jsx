8fc9e04babd500bf08e210e4a378414bda4b03e1
desaihd <hetav.desai20@gmail.com>
2021-06-15 01:11:40 +0530

    add empty feed message

--- src/features/posts/Feed.jsx
+++ src/features/posts/Feed.jsx
@@ -1,15 +1,26 @@
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { PostCard } from "../../components";
 
 export default function Feed() {
+  const { user } = useSelector((state) => state.auth);
   const { posts } = useSelector((state) => state.posts);
+
+  console.log(user);
+
+  const feedPosts = posts?.filter(
+    (post) => !post.parent_post && user?.following?.includes(post.user_id)
+  );
 
   return (
     <div className="pt-4 pb-20">
-      {posts
-        .filter((post) => !post.parent_post)
-        .map((post, index) => (
+      {posts.length !== 0 && feedPosts.length === 0 && (
+        <p className="font-semibold text-xl text-center w-full my-6">
+          No posts to display
+        </p>
+      )}
+      {user &&
+        feedPosts &&
+        feedPosts.map((post, index) => (
           <PostCard key={`${post.id}-${index}`} post={post} />
         ))}
     </div>
